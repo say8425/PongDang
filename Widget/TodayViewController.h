7db4dbc630dc64a6580d3a08dc05d21b9f01ca94
Penguin <say8425@gmail.com>
2015-07-16 22:48:58 +0900

    Widget is added and basic interface is builed

---
+++ Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Widget
+//
+//  Created by Cheon Park on 2015. 7. 16..
+//  Copyright (c) 2015년 northPenguin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
