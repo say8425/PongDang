b929229b5cad568bb3cea75b8d2cbe0b2a4c3bb7
Penguin <say8425@gmail.com>
2015-12-25 22:14:43 +0900

    pod 'M13ProgressSuite', '~> 1.2' installed

--- Podfile
+++ Podfile
@@ -2,7 +2,7 @@ platform :ios, '7.0'
 use_frameworks!
 
 target 'PongDang' do
-
+  pod 'M13ProgressSuite', '~> 1.2'
 end
 
 target 'PongDangTests' do
---
+++ Pods/Target Support Files/Pods-PongDang/Pods-PongDang-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PongDang : NSObject
+@end
+@implementation PodsDummy_Pods_PongDang
+@end
